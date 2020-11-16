b244bdba404120c094191fefe4ae32a02d973f44
TinusKG <74502228+TinusKG@users.noreply.github.com>
2020-11-16 18:27:18 +0100

    Update Lab3oppg1.py

--- Lab3oppg1.py
+++ Lab3oppg1.py
@@ -7,23 +7,19 @@ import matplotlib.pyplot as plt
 G=9.81
 R=0.54
 r=0.03
-m=0.5
+m=1 #Massen er kun nødvendig for utregning av energi. Kan være hva som helst.
 
 #Startsbetingelser
 t_start=0
 t_slutt=2
 t_delta=0.0001
-t_verdier=np.linspace(t_start,t_slutt,int(1/t_delta))
+t_verdier=np.linspace(t_start,t_slutt,int(t_slutt/t_delta))
 
-theta_init=0.05 #Her må vi ha theta>0 slik at løkken ikke repeterer tomme lister
-v_init=0
-tol=0.1 #Toleransen er avhengig av t_delta. 
+theta_init=0.5 #Her må vi ha en vinkel>0 slik at løkken ikke repeterer tomme lister
+tol=0.01 #Toleransen er avhengig av t_delta. 
         #Jo mindre t_delta, altså flere iterasjoner, jo mindre kan man sette tol, og man får et mer nøyaktig svar.
 
 #Lager tomme lister for alle verdiene
-N=np.zeros(int(1/t_delta))
-f=np.zeros(int(1/t_delta))
-
 a_verdier=np.zeros(len(t_verdier))
 v_verdier=np.zeros(len(t_verdier))
 
@@ -41,7 +37,7 @@ v_verdier[0]=0
 omega_verdier[0]=0
 
 #Løkken fyller de tomme listene for akselerasjon, vinkel, translatorisk fart og vinkelfart.
-#Alle disse listene er avhengige av startsbetingelsene og verdier fra forrige iterasjon.
+#Alle disse listene er avhengige av startsbetingelsene og verdier fra forrige iterasjon
 
 for i in range(len(t_verdier)-1):
     
@@ -56,24 +52,30 @@ for i in range(len(t_verdier)-1):
     if abs(G*np.cos(theta_verdier[i+1])-(v_verdier[i+1]**2/(R+r)))<tol:
         #Løkken stopper dersom normalkraften blir mindre enn toleransen
         print("Kritisk verdi: ",round(theta_verdier[i+1]*180/np.pi,4),"grader")
+        z=i+1
         break
-        
+    
 #Utregning for den totale energien
 EK_trans=0.5*m*(v_verdier**2)
 EP=m*G*(R+r)*np.cos(theta_verdier)
 
-#Graf av vinkelen over det gitte tidsintervallet. 
+#Grafer vinkelen over tidsintervallet
 plt.figure(0)
-plt.plot(t_verdier,theta_verdier*180/np.pi,label="Theta verdier")
+plt.plot(t_verdier[0:z+1],theta_verdier[0:z+1]*180/np.pi,color='black',label="Theta verdier")
 plt.xlabel("Tid")
 plt.ylabel("Vinkel (grader)")
+plt.grid(True)
 plt.legend()
 plt.show()
 
+#Grafer den totale energien
 plt.figure(0)
-plt.plot(t_verdier,(EK_trans+EP),color='orange',label="Total energi")
-plt.xlabel("Tid")
+plt.plot(t_verdier[0:z+1],(EK_trans[0:z+1]+EP[0:z+1]),color='orange',label="Total energi (J)")
+plt.plot(t_verdier[0:z+1],EK_trans[0:z+1],color='purple',label="Kinetisk energi (J)")
+plt.plot(t_verdier[0:z+1],EP[0:z+1],color='green',label="Potensiell energi (J)")
+plt.grid(True)
+plt.xlabel("Tid (s)")
 plt.ylabel("Energi (J)")
-plt.axis([0,2,0,4])
+plt.axis([0,t_verdier[z+1],0,10]) #Her kan man bestemme aksene for energi-grafen
 plt.legend()
 plt.show()
